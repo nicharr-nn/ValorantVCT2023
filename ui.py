1182c27df585a9e485065fc9fb94818e9ffd9064
Nicha RUANGRIT <nicha.rua@ku.th>
2024-05-03 21:15:50 +0700

    added more widget

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
 
 class AppUI(tk.Tk):
     def __init__(self):
@@ -8,8 +9,7 @@ class AppUI(tk.Tk):
         self.title("VALORANT Champions Tour 2023 Player Performance")
         self.__screen_width = self.winfo_screenwidth()
         self.__screen_height = self.winfo_screenheight()
-        self.geometry(f"{self.__screen_width//2}x{self.__screen_height//2}")
-        self.load_data()
+        self.geometry(f"{self.__screen_width}x{self.__screen_height}")
         self.main_frame = ttk.Frame(self)
         self.init_components()
 
@@ -19,9 +19,11 @@ class AppUI(tk.Tk):
         self.home_btn = ttk.Button(self, text="Home", command=self.home_page)
         self.exit_btn = ttk.Button(self, text="Exit", command=self.quit)
 
-        self.display_label.grid(row=0, column=0, columnspan=3, sticky="nsew")
-        self.home_btn.grid(row=1, column=0, sticky="nsew")
-        self.exit_btn.grid(row=2, column=0, sticky="nsew")
+        self.display_label.place(relx=0.5, rely=0.4, anchor="center")
+        self.home_btn.place(relx=0.5, rely=0.5, anchor="center")
+        self.exit_btn.place(relx=0.5, rely=0.6, anchor="center")
+        # self.home_btn.grid(row=1, column=0, sticky="nsew")
+        # self.exit_btn.grid(row=2, column=0, sticky="nsew")
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
@@ -29,17 +31,43 @@ class AppUI(tk.Tk):
         self.grid_columnconfigure(0, weight=1)
 
     def home_page(self):
-        self.display_label.destroy()
-        self.home_btn.destroy()
-        self.exit_btn.destroy()
+        self.remove_widgets()
 
-        self.display_label = ttk.Label(self, text="Welcome!",
+        self.display_label = ttk.Label(self, text="Choose the players' statistics",
                                        font=("Arial", 16, "bold"))
-        self.display_label.grid(row=0, column=0, columnspan=3, sticky="nsew")
+        self.display_label.place(relx=0.5, rely=0.4, anchor="center")
+        self.overall_btn = ttk.Button(self, text="Overall")
+        self.overall_btn.bind('<Button>', self.observer)
+        self.byagent_btn = ttk.Button(self, text="By Agent")
+        self.byagent_btn.bind('<Button>', self.observer)
 
-    def load_data(self):
-        # read_csv
+        self.overall_btn.place(relx=0.4, rely=0.5, anchor="center")
+        self.byagent_btn.place(relx=0.6, rely=0.5, anchor="center")
+
+    def statistic_page(self, event):
+        key_pressed = event.widget.cget('text')
+        self.remove_widgets()
+        self.load_data(key_pressed)
+
+        self.back_btn = ttk.Button(self, text="Back", command=self.home_page)
+        self.story_telling_btn = ttk.Button(self, text="Story Telling", command=self.story_page)
+        self.back_btn.place(relx=0.9, rely=0.9, anchor="se")
+
+    def story_page(self):
         pass
+
+    def load_data(self, data):
+        if data == "Overall":
+            overall_data = pd.read_csv("overall_player_stats.csv")
+        elif data == "By Agent":
+            by_agent_data = pd.read_csv("players_stats_by_agent.csv")
+
+    def observer(self, event):
+        self.statistic_page(event)
+
+    def remove_widgets(self):
+        for widget in self.winfo_children():
+            widget.destroy()
 
     def run(self):
         """Display the calculator user interface."""
