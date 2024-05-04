87d31070b88cad05b889cc19009e1d26c2144003
Nicha RUANGRIT <nicha.rua@ku.th>
2024-05-04 20:06:26 +0700

    add combobox and select preview

--- ui.py
+++ ui.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
+from graph import Graph
 
 class AppUI(tk.Tk):
-    def __init__(self):
+    def __init__(self, df=''):
         """Create a new instance of the AppUI class."""
         super().__init__()
         self.title("VALORANT Champions Tour 2023 Player Performance")
@@ -11,6 +12,8 @@ class AppUI(tk.Tk):
         self.__screen_height = self.winfo_screenheight()
         self.geometry(f"{self.__screen_width}x{self.__screen_height}")
         self.main_frame = ttk.Frame(self)
+        self.__df = df
+        self.selected_data = []
         self.init_components()
 
     def init_components(self):
@@ -55,6 +58,23 @@ class AppUI(tk.Tk):
         self.story_telling_btn = ttk.Button(self, text="Story Telling", command=self.story_page)
         self.story_telling_btn.grid(row=0, column=0, sticky="nw", padx=30, pady=10)
 
+        # select the data to display a graph bar, histogram, boxplot, scatter, and pie
+        self.cbb_chart = ttk.Combobox(self, values=["Bar", "Histogram", "Boxplot", "Scatter", "Pie"])
+        self.cbb_chart.place(relx=0.7, rely=0.9, anchor="se")
+
+        if key_pressed == "Overall":
+            self.cbb_column = ttk.Combobox(self, values=["Number of Agents Played","Rounds Played","Rating","ACS","KD","ADR","KPR","APR","FKPR","FDPR","Kills Max","K","D","A","FK","FD"])
+        elif key_pressed == "By Agent":
+            self.cbb_column = ttk.Combobox(self, values=["Rounds Played","Rating","ACS","KD","KAST","ADR","KPR","APR","FKPR","FDPR","HSP","CSP","Kills Max","K","D","A","FK","FD"])
+        self.cbb_column.place(relx=0.5, rely=0.9, anchor="se")
+
+
+        self.graph_btn = ttk.Button(self, text="Process", command=self.graph_page)
+        self.graph_btn.place(relx=0.8, rely=0.9, anchor="se")
+
+    def graph_page(self):
+        self.remove_widgets()
+
 
     def story_page(self):
         self.remove_widgets()
@@ -75,8 +95,10 @@ class AppUI(tk.Tk):
             widget.destroy()
 
     def display_table(self, data):
-        self.frame = tk.Frame(self, width=self.__screen_width//2, height=self.__screen_width//2)
+        self.frame = tk.Frame(self, width=self.__screen_width, height=self.__screen_height // 4)
         self.frame.grid(row=0, column=0, padx=30, pady=20)
+        self.frame2 = tk.Frame(self, width=self.__screen_width, height=self.__screen_height // 4, bg="black")
+        self.frame2.grid(row=1, column=0, padx=30, pady=20)
 
         y_scrollbar = tk.Scrollbar(self.frame, orient="vertical")
         y_scrollbar.pack(side="right", fill="y")
@@ -98,6 +120,32 @@ class AppUI(tk.Tk):
         y_scrollbar.config(command=self.tree.yview)
         x_scrollbar.config(command=self.tree.xview)
 
+        # second frame
+
+        y_scrollbar2 = tk.Scrollbar(self.frame2, orient="vertical")
+        y_scrollbar2.pack(side="right", fill="y")
+
+        x_scrollbar2 = tk.Scrollbar(self.frame2, orient="horizontal")
+        x_scrollbar2.pack(side="bottom", fill="x")
+
+        self.tree2 = ttk.Treeview(self.frame2, columns=tuple(data.columns), show="headings")
+
+        for col in data.columns:
+            self.tree2.heading(col, text=col)
+            self.tree2.column(col, width=100, anchor="center")
+
+        self.tree2.pack(side="left", fill="both", expand=True)
+
+        self.tree2.configure(yscrollcommand=y_scrollbar2.set, xscrollcommand=x_scrollbar2.set)
+        y_scrollbar2.config(command=self.tree2.yview)
+        x_scrollbar2.config(command=self.tree2.xview)
+
+        self.tree.bind("<ButtonRelease-1>", self.select_item)
+
+    def select_item(self, data):
+        str_data = self.tree.item(self.tree.selection())
+        self.selected_data.append(str_data['values'])
+        self.tree2.insert("", "end", values=str_data['values'])
 
 
     def run(self):
