42be8d5690d214c23cfd3041fc7ba770afc77c18
Nicha RUANGRIT <nicha.rua@ku.th>
2024-05-04 21:45:40 +0700

    added class

--- graph.py
+++ graph.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 class Graph:
-    def __init__(self):
+    def __init__(self, df=None, selected_column=None, title=None):
+        pass
+
+    def bar_processor(self):
         pass
