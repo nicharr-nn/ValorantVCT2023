6886130ca2a4490f34e32870ce8fb76597527dfa
Nicha RUANGRIT <nicha.rua@ku.th>
2024-05-09 20:06:20 +0700

    add bar graph and pie graph processor

--- graph.py
+++ graph.py
@@ -1,10 +1,39 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class Graph:
-    def __init__(self, df=None, selected_column=None, title=None):
-        pass
+    def __init__(self, df=None, title=None, data=None):
+        self.df = df
+        self.data = data
+        self.title = title
 
-    def bar_processor(self):
-        pass
+    def bar_processor(self, df, title, data):
+        window = tk.Toplevel(self.df)
+        window.title(title)
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        fig.set_size_inches(5, 4)
+
+        sns.barplot(x='Player', y=title, data=df, ax=ax)
+        ax.set_xticklabels(ax.get_xticklabels(), horizontalalignment='right')
+        ax.set_title(title)
+
+        canvas = FigureCanvasTkAgg(fig, master=window)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def pie_processor(self, df, title, data):
+        window = tk.Toplevel(self.df)
+        window.title("Pie Chart")
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        fig.set_size_inches(5, 4)
+
+        ax.pie(df[title], labels=df['Player'], autopct='%1.2f%%')
+        ax.set_title(title)
+
+        canvas = FigureCanvasTkAgg(fig, master=window)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
