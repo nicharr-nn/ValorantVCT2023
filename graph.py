ccc81bc7364f05f3b37183393aa0a29a2202f1de
Nicha RUANGRIT <nicha.rua@ku.th>
2024-05-09 23:19:41 +0700

    added boxplot and edited bar processor

--- graph.py
+++ graph.py
@@ -14,11 +14,14 @@ class Graph:
         window.title(title)
         fig = plt.figure()
         ax = fig.add_subplot()
-        fig.set_size_inches(5, 4)
+        fig.set_size_inches(4, 5)
 
         sns.barplot(x='Player', y=title, data=df, ax=ax)
-        ax.set_xticklabels(ax.get_xticklabels(), horizontalalignment='right')
-        ax.set_title(title)
+        ax.set_xticks(range(len(df['Player'])))
+        ax.set_xticklabels(df['Player'], rotation=90, ha='right', fontsize=8)
+        ax.set_yticks(ax.get_yticks())
+        ax.set_yticklabels(ax.get_yticklabels(), fontsize=8)
+        ax.set_title(title, fontsize=8)
 
         canvas = FigureCanvasTkAgg(fig, master=window)
         canvas.draw()
@@ -46,6 +49,20 @@ class Graph:
         fig.set_size_inches(5, 4)
 
         sns.histplot(data=df, x=title, ax=ax)
+        ax.set_title(title)
+
+        canvas = FigureCanvasTkAgg(fig, master=window)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def boxplot_processor(self, df, title, data):
+        window = tk.Toplevel(self.df)
+        window.title("Boxplot")
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        fig.set_size_inches(5, 4)
+
+        sns.boxplot(data=df, x=title, ax=ax)
         ax.set_title(title)
 
         canvas = FigureCanvasTkAgg(fig, master=window)
